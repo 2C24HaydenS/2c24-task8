7f367cabb0bf6548074370758bbbb6b48407d3ac
2C24HaydenS <115450543+2C24HaydenS@users.noreply.github.com>
2022-11-13 15:33:53 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 input.onButtonPressed(Button.A, function () {
-    if (input.compassHeading() < 180) {
+    if (input.compassHeading() > 180) {
         music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 5000, 255, 0, 1500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
     } else {
-        music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 218, 255, 0, 1500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 1645, 1, 255, 0, 1500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
     }
     basic.showString("" + (input.compassHeading()))
 })
